99c994838de0e49b0f619aa1f057ec45a61454f6
Aliyare Warsame <caliyare712@gmail.com>
2022-07-03 09:30:54 -0700

    sdfghj

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,13 @@
 *
 *  * main - Entry point
 *
-*   * 
+*   *
 *
 *    * Description: 'the program's description'
 *
-*     * 
+*    * 
 *
-*      * Return: Always 0 (Success)
+*    * Return: Always 0 (Success)
 *
 *       */ 
 
