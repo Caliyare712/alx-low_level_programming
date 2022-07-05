788601280b4d47f8ff06bb105753beecc7a5641a
Aliyare Warsame <caliyare712@gmail.com>
2022-07-05 09:32:38 -0700

    final

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  *  * print_sign - a function that prints the sign of a number
  *   * @n: number input
- *    * Return: prints '+' if n > 0, prints '0' if n = 0, -1 prints '-' if n < 0
+ *    * Return: prints '+' if n > 0, if n = 0, -1 prints '-' if n < 0
 */
 
 int print_sign(int n)
