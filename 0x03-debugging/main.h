fa50fcd8dd35904561b2c179a026f5a2e04c6061
Aliyare Warsame <caliyare712@gmail.com>
2022-07-06 01:17:38 -0700

    lll

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,6 @@
 
 
 
-void positive_or_negative(int i);
-
 void print_alphabet_x10(void);
 
 int _islower(int c);
@@ -28,6 +26,6 @@ void print_to_98(int n);
 
 void print_times_table(int n);
 
-
+void positive_or_negative(int i);
 
 #endif /* MAIN_H */
