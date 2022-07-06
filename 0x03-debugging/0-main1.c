275275388e0531cc3eec1061335cb767a06e88c9
Aliyare Warsame <caliyare712@gmail.com>
2022-07-06 00:55:37 -0700

    aa

--- 0x03-debugging/0-main1.c
+++ 0x03-debugging/0-main1.c
@@ -32,7 +32,43 @@ int main(void)
 
 
 
-	positive_or_negative(0);
+	int n;
+
+
+
+
+
+
+
+	srand(time(0));
+
+
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+
+	{
+
+	printf("%d is positive\n", n);
+
+	}
+
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+
+	}
+
+
+
+
+
 	return (0);
 
 
