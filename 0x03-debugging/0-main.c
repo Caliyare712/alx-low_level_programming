70de21bba6f6d8bcfe3b60cc30308c29d724b08a
Aliyare Warsame <caliyare712@gmail.com>
2022-07-06 01:25:26 -0700

    yyy

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,18 @@
 #include "main.h"
 
 
-
-
 /**
- *  *  *main -> assign a random number to the variable nexecuted
- *   *   *and print the last digit of the number stored in the variable n
+ *  *  *main -> test positive_or_negative
  *    *    *Return: Always 0 (Success)
 */
 
 
-
 int main(void)
 
-
-
 {
-
 	int i=0;
 
 	positive_or_negative(i);
 
 	return (0);
-
 }
