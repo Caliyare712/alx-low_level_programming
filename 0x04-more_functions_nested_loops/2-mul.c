aa9377a7dd019ed37a65fe5202be5788ab416990
Aliyare Warsame <caliyare712@gmail.com>
2022-07-07 10:29:05 -0700

    1-aaa

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
-/**
- *  * main - multiplies var a and b
- *   * @a: a -  Variable
- *    * @b: b - variable
- *     * Return: Always 0.
- *      */
 
-int mul(int a, int b)
+
+/**
+ *   * mul - Multiplies two integers
+ *     * @x: The first number to multiply
+ *       * @y: The second number to multiply with first number
+ *         *
+ *           * Return: The result of multiplication between x param and y param
+*/
+
+int mul(int x, int y)
+
 {
 
-	return (a * b);
+	return (x * y);
 
 }
