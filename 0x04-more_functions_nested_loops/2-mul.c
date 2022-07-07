c276e8ee5a88e13a9b1c5a76d6fde3112b1a5170
Aliyare Warsame <caliyare712@gmail.com>
2022-07-07 09:52:46 -0700

    task 2 error

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - prints the multiplication of integers
- * var a
- * var b
- * return: 0
-*/
+ *  * mul -  checks for checks for a digit (0 through 9).
+ *   * @a: a -  Variable
+ *    * @b: b - variable
+ *     * Return: Always 0.
+ *      */
 
 int mul(int a, int b)
 {
