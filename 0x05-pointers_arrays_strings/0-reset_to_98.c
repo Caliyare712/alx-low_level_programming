47788d02ec2cfc067b081395a2edf89a35db57ba
Aliyare Warsame <caliyare712@gmail.com>
2022-07-11 07:37:05 -0700

    task 0 update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  * reset_to_ - Reset the variable sent to the function at 98
+ *  * reset_to_98 - Reset the variable sent to the function at 98
  *   * @n: Pointer to the integer sent to the function
  *     * Return: Nothing
 */
