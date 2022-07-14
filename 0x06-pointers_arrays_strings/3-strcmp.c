2582d077c31fbe2b70cdec3eeef51b7d236447c9
Aliyare Warsame <caliyare712@gmail.com>
2022-07-14 02:40:43 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,40 +1,46 @@
 #include "main.h"
 
 /**
- *  * _strcmp - compares two strings
- *    * @s1: first string
- *     * @s2: second string
- *       * Return: -15 if s1 < s2
- *        *          0  if s1 = s2
- *         *          15 if s1 > s2
+ *   * _strcmp - Compares two strings
+ *     * @s1: The first string
+ *       * @s2: The second string
+ *           * Return: int value
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1Length = 0, s2Length = 0;
-	int rtrnValNeg = -10;
-	int rtrnValPos = 10;
-	int smallerLength = 0;
-	while (*(s1 + s1Length) != '\0')
-		s1Length++;
-	while (*(s2 + s2Length) != '\0')
-		s2Length++;
-	smallerLength = (s1Length < s2Length) ? s1Length : s2Length;
-	rtrnValNeg += -(smallerLength);
-	rtrnValPos += smallerLength;
-	if (s1Length < s2Length)
-		return (rtrnValNeg);
-	if (s1Length > s2Length)
-		return (rtrnValPos);
+	int a = 0, b = 0, c = 0, r = 0, lim;
 
-	while (*s1) /*same length, so loop through one and check both */
+	while (s1[a])
 	{
-		if (*s1 < *s2)
-			return (rtrnValNeg);
-		if (*s1 > *s2)
-			return (rtrnValPos);
-		s1++;
-		s2++;
+		a++;
 	}
-	return (0); /* equal */
+	while (s2[b])
+	{
+		b++;
+	}
+	if (a <= b)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+		c++;
+	}
+	return (r);
 }
