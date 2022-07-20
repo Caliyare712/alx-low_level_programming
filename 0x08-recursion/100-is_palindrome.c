d349efd256c247c620a94f5027fcc4cbf69b9ab7
Aliyare Warsame <caliyare712@gmail.com>
2022-07-20 04:06:55 -0700

    dfh

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -18,67 +18,37 @@ int _strlen_recursion(char *s)
 
 
 /**
- *
  *  * p1 - palindrome
- *
  *   * @s: pointer to string
- *
  *    * @l: position
- *
  *     * Return: boolena
- *
- *      */
-
-
+*/
 
 int p1(char *s, int l)
-
 {
+	if (l < 1)
+	{
+		return (1);
+	}
 
-		if (l < 1)
-
-				{
-
-							return (1);
-
-								}
-
-
-
-			if (*s == *(s + l))
-
-					{
-
-								return (p1(s + 1, l - 2));
-
-									}
-
-				return (0);
-
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
 }
 
 
 
 /**
- *
  *  * is_palindrome - palindrome
- *
  *   * @s: pointer to string
- *
  *    * Return: recursion
- *
- *     */
-
-
+*/
 
 int is_palindrome(char *s)
-
 {
+	int len = _strlen_recursion(s);
 
-		int len = _strlen_recursion(s);
-
-
-
-			return (p1(s, len - 1));
-
+	return (p1(s, len - 1));
 }
