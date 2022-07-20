f60e28870cebef24a260f972046ed6ef7aa99067
Aliyare Warsame <caliyare712@gmail.com>
2022-07-20 04:08:40 -0700

    shfhd

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,43 +18,23 @@ int _strlen_recursion(char *s)
 
 
 /**
- *
  *  * p1 - palindrome
- *
  *   * @s: pointer to string
- *
  *    * @l: position
- *
  *     * Return: boolena
- *
- *      */
-
-
+*/
 
 int p1(char *s, int l)
-
 {
-
-		if (l < 1)
-
-				{
-
-							return (1);
-
-								}
-
-
-
-			if (*s == *(s + l))
-
-					{
-
-								return (p1(s + 1, l - 2));
-
-									}
-
-				return (0);
-
+	if (l < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
 }
 
 
