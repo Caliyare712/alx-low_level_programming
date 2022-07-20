3fcea96f327d5a8781e6fa89f8f05032802037f9
Aliyare Warsame <caliyare712@gmail.com>
2022-07-20 04:11:03 -0700

    aaa

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,8 +15,6 @@ int _strlen_recursion(char *s)
 	return (1 + _strlen_recursion(++s));
 }
 
-
-
 /**
  *  * p1 - palindrome
  *   * @s: pointer to string
@@ -37,28 +35,15 @@ int p1(char *s, int l)
 	return (0);
 }
 
-
-
 /**
- *
  *  * is_palindrome - palindrome
- *
  *   * @s: pointer to string
- *
  *    * Return: recursion
- *
- *     */
-
-
+*/
 
 int is_palindrome(char *s)
-
 {
+	int len = _strlen_recursion(s);
 
-		int len = _strlen_recursion(s);
-
-
-
-			return (p1(s, len - 1));
-
+	return (p1(s, len - 1));
 }
